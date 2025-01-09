227055241308ad76f4b2ba98f823dfc60a03ef07
Nika M. <merabishvilinikusha@gmail.com>
2025-01-09 10:23:54 +0400

    refactor: removed typography from app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "./helpers/theme/theme";
-import Typography from "@mui/material/Typography";
 
 export function App() {
   return <ThemeProvider theme={theme}></ThemeProvider>;
