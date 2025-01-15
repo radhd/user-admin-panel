9b6360188f7d733b53bfa3b3a83bc861e96a3705
Nika M. <merabishvilinikusha@gmail.com>
2025-01-15 10:10:10 +0400

    refactor: AP-13-Add user logic to add locally

--- src/pages/UsersManagementPage/components/AddDialogUserForm/hooks/useDialogForm.ts
+++ src/pages/UsersManagementPage/components/AddDialogUserForm/hooks/useDialogForm.ts
@@ -1,10 +1,15 @@
 import { USER_ADD_FORM_DEFAULT_VALUES } from "../../../../../components/organisms/AddDialogForm/constants/userDefaultFormValues";
 import { schema } from "../../../../../components/organisms/AddDialogForm/utils";
 import { useAPControlledInput, useAPDialog } from "@/components/atoms";
-import { useAddUserMutation } from "@/services";
+import { setUsers, TUserRole, useAddUserMutation } from "@/services";
 import { TFormData } from "../../../../../components/organisms/AddDialogForm/TFormData.types";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "@/services/setupStore";
+import { ITransformedUserResponse } from "@/services/users/types";
 
 export const useDialogForm = () => {
+  const dispatch = useDispatch();
+  const users = useSelector((state: RootState) => state.users.users);
   const [addUser, { isLoading, isSuccess, reset: resetMutation }] =
     useAddUserMutation();
   const { open, handleClickOpen, handleClose } = useAPDialog();
@@ -28,7 +33,20 @@ export const useDialogForm = () => {
     try {
       const { firstName, lastName, email, age: stringAge, role } = data;
       const age = Number(stringAge);
-      await addUser({ firstName, lastName, email, age, role }).unwrap();
+      const newUser = await addUser({
+        firstName,
+        lastName,
+        email,
+        age,
+        role,
+      }).unwrap();
+
+      const transformedUser: ITransformedUserResponse = {
+        ...newUser,
+        role: role as TUserRole,
+      };
+
+      dispatch(setUsers([...users, transformedUser]));
       reset(USER_ADD_FORM_DEFAULT_VALUES);
       handleClose();
     } catch (error) {
